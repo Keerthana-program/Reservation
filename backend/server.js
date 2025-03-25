977ffa20cf9393291355f2726a4a92f105fb71e9
keerthana-program <keerthanapollachi99@gmail.com>
2025-03-25 20:41:00 +0530

    Task 3

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5173"], // Allow frontend to connect
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://musical-sfogliatella-e81d3e.netlify.app"], // Allow frontend to connect
     methods: ["GET", "POST"]
   },
 });
